23173a43c1c0649f8f909c1348ec56a76d51194f
Ankit Jailwal <jailwalankit@gmail.com>
2021-06-08 13:08:20 +0530

    Created class to collect images of all type(Network/Asset) in one list.

--- lib/service/grofers_carousel_package.dart
+++ lib/service/grofers_carousel_package.dart
@@ -10,6 +10,7 @@ class grofers_carousel extends StatefulWidget {
   final bool autoplay;
   final Duration autoplayDuration;
   final void Function(int) onImageTap;
+  final void Function(int, int) onImageChange;
 
   grofers_carousel({
     this.images,
@@ -19,6 +20,7 @@ class grofers_carousel extends StatefulWidget {
     this.autoplayDuration = const Duration(seconds: 2),
     this.onImageTap,
     this.defaultImage,
+    this.onImageChange,
   });
   @override
   _grofers_carouselState createState() => _grofers_carouselState();
@@ -27,6 +29,55 @@ class grofers_carousel extends StatefulWidget {
 class _grofers_carouselState extends State<grofers_carousel> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final List<Widget> listImages = (widget.images != null && widget.images.isNotEmpty)
+        ? widget.images.map<Widget>(
+          (netImage) {
+        if (netImage is ImageProvider) {
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+              image: DecorationImage(
+                image: netImage,
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        } else if (netImage is FadeInImage) {
+          return ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.center,
+                    stops: [0.0, 0.5],
+                    colors: [Colors.grey[800].withOpacity(1.0),
+                             Colors.grey[800].withOpacity(0.0)
+                    ],
+                  ),
+                ),
+                child: netImage),
+          );
+        } else {
+          return netImage;
+        }
+      },
+    ).toList()
+        : [Container(
+        decoration: BoxDecoration(
+          borderRadius:BorderRadius.all( Radius.circular(8.0)),
+          image: DecorationImage(
+            //colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.dstATop),
+            image: widget.defaultImage,
+            fit: BoxFit.cover,
+          ),
+        ),
+      )
+      ,
+    ];
+    return
+        Container(
+
+        );
   }
 }
