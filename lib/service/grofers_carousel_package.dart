48447b963892973a1331f557f873f5081375e6b8
Ankit Jailwal <jailwalankit@gmail.com>
2021-06-08 12:27:05 +0530

    Creating class image carousel

---
+++ lib/service/grofers_carousel_package.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class grofers_carousel extends StatefulWidget {
+  @override
+  _grofers_carouselState createState() => _grofers_carouselState();
+}
+
+class _grofers_carouselState extends State<grofers_carousel> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
