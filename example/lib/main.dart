89afe98324318f69af1c03c984409f98f17dca5a
Ankit Jailwal <jailwalankit@gmail.com>
2021-06-08 22:15:15 +0530

    Changes in splashScreen

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:example/screen/homePage.dart';
 import 'package:flutter/material.dart';
-import 'package:grofers_carousel/grofers_carousel.dart';
 import 'package:splashscreen/splashscreen.dart';
 
 void main() {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: true,
+    debugShowCheckedModeBanner: false,
     title: 'Grofers Image Carousel',
     home: GrofersCarouselPage(),
   ));
@@ -14,15 +13,17 @@ void main() {
 class GrofersCarouselPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SplashScreen(
-        seconds: 3,
-        navigateAfterSeconds: new homePage(),
-        title: new Text('Welcome In Grofers',style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w600),),
-        image: new Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Grofers_Logo.png/1200px-Grofers_Logo.png'),
-        backgroundColor: Color(0xfff25c2a),
-        styleTextUnderTheLoader: new TextStyle(),
-        photoSize: 100.0,
-        loaderColor: Colors.white
+    return SafeArea(
+      child: SplashScreen(
+          seconds: 3,
+          navigateAfterSeconds: new homePage(),
+          title: new Text('Welcome In Grofers',style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w500),),
+          image: new Image.asset('assets/grofersLogo.png'),
+          backgroundColor: Color(0xfff27437),
+          styleTextUnderTheLoader: new TextStyle(),
+          photoSize: 100.0,
+          loaderColor: Colors.white
+      ),
     );
   }
 }
--- example/lib/screen/homePage.dart
+++ example/lib/screen/homePage.dart
@@ -9,18 +9,20 @@ class homePage extends StatefulWidget {
 class _homePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: grofers_carousel(
-          carousalPadding: 4,
-          autoplay: true,
-          animationCurve: Curves.fastOutSlowIn,
-          animationDuration: Duration(milliseconds: 1000),
-          images: [
-            NetworkImage('https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=50,w=954,h=477/layout-engine/2021-06/Homepage_Household-laundry-_8.jpg'),
-            NetworkImage('https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=50,h=274/layout-engine/2021-06/SSF_june21_masthead2_0.jpg'),
-            NetworkImage('https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=50,w=954,h=477/layout-engine/2021-06/HPB-RESIZE-JUNE.jpg'),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: grofers_carousel(
+            carousalPadding: 4,
+            autoplay: true,
+            animationCurve: Curves.fastOutSlowIn,
+            animationDuration: Duration(milliseconds: 1000),
+            images: [
+              NetworkImage('https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=50,w=954,h=477/layout-engine/2021-06/Homepage_Household-laundry-_8.jpg'),
+              NetworkImage('https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=50,h=274/layout-engine/2021-06/SSF_june21_masthead2_0.jpg'),
+              NetworkImage('https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=50,w=954,h=477/layout-engine/2021-06/HPB-RESIZE-JUNE.jpg'),
+            ],
+          ),
         ),
       ),
     );
