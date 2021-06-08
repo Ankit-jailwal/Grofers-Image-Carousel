d8aa2c43e55c5de98a952d1d69b0f5c0d4234c71
Ankit Jailwal <jailwalankit@gmail.com>
2021-06-08 22:37:41 +0530

    Basic implementation of grofers_carousel widget

--- example/lib/screen/homePage.dart
+++ example/lib/screen/homePage.dart
@@ -9,20 +9,30 @@ class homePage extends StatefulWidget {
 class _homePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: grofers_carousel(
-            carousalPadding: 4,
-            autoplay: true,
-            animationCurve: Curves.fastOutSlowIn,
-            animationDuration: Duration(milliseconds: 1000),
-            images: [
-              NetworkImage('https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=50,w=954,h=477/layout-engine/2021-06/Homepage_Household-laundry-_8.jpg'),
-              NetworkImage('https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=50,h=274/layout-engine/2021-06/SSF_june21_masthead2_0.jpg'),
-              NetworkImage('https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=50,w=954,h=477/layout-engine/2021-06/HPB-RESIZE-JUNE.jpg'),
-            ],
-          ),
+        body: grofers_carousel(
+          carousalPadding: 5,
+          carousalHeight: screenSize.height*0.4,
+          carousalWidth: screenSize.width,
+          carousalRadius: 80,
+          autoplay: false,
+          animationCurve: Curves.fastOutSlowIn,
+          animationDuration: Duration(seconds: 1),
+          autoplayDuration: Duration(seconds: 2),
+          onTap: (index){
+            //TODO space to implement any function
+          },
+          onChange: (prevIndex,nextIndex){
+            //TODO space to implement any function
+          },
+          images: [
+            NetworkImage('https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=50,w=954,h=477/layout-engine/2021-06/Homepage_Household-laundry-_8.jpg'),
+            NetworkImage('https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=50,h=274/layout-engine/2021-06/SSF_june21_masthead2_0.jpg'),
+            NetworkImage('https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=50,w=954,h=477/layout-engine/2021-06/HPB-RESIZE-JUNE.jpg'),
+          ],
+          defaultImage: NetworkImage('http://fpae.pt/backup/20181025/wp/wp-content/plugins/post-slider-carousel/images/no-image-available-grid.jpg'), //This will be used when there is no images in above list
         ),
       ),
     );
