3f84dc29338ab59f31a9e2d117544ddfd8f56333
Ankit Jailwal <jailwalankit@gmail.com>
2021-06-08 12:51:20 +0530

    Declaring methods for carousel package

--- lib/service/grofers_carousel_package.dart
+++ lib/service/grofers_carousel_package.dart
@@ -3,6 +3,23 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class grofers_carousel extends StatefulWidget {
+  final List images;
+  final defaultImage;
+  final Curve animationCurve;
+  final Duration animationDuration;
+  final bool autoplay;
+  final Duration autoplayDuration;
+  final void Function(int) onImageTap;
+
+  grofers_carousel({
+    this.images,
+    this.animationCurve = Curves.ease,
+    this.animationDuration = const Duration(milliseconds: 300),
+    this.autoplay = true,
+    this.autoplayDuration = const Duration(seconds: 2),
+    this.onImageTap,
+    this.defaultImage,
+  });
   @override
   _grofers_carouselState createState() => _grofers_carouselState();
 }
